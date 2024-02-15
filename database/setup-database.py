69a8ca3b0f91e723232ecb8ff825f5b9d6e36a89
Karan Anand <thekarananand@gmail.com>
2024-02-15 21:34:47 +0530

    feat: replace html by md

--- database/setup-database.py
+++ database/setup-database.py
@@ -13,12 +13,9 @@ sampledata = [
     {
         'title': 'How to Hack Google',
         'author': 'Karan Anand',
-        'article_html_content': """
+        'article_html_content': """# I don't Know
 
-        <h2>I don't Know</h2>
-
-        <p>Water, the elixir of life. We gulp it down, splash in it, even cook with it, often taking its purity for granted. But lurking beneath its seemingly innocuous surface, a world of complexity unfolds, where unseen contaminants can wreak havoc on our health and well-being. So, what does "pure" water truly mean? And can we ensure we're getting the good stuff?</p>
-
+Water, the elixir of life. We gulp it down, splash in it, even cook with it, often taking its purity for granted. But lurking beneath its seemingly innocuous surface, a world of complexity unfolds, where unseen contaminants can wreak havoc on our health and well-being. So, what does "pure" water truly mean? And can we ensure we're getting the good stuff?
         """,
         'table_of_content': [
             "# I don't Know"
@@ -27,39 +24,32 @@ sampledata = [
     {
         'title': 'H2: H2O, Oh No! Unraveling the Mysteries of Water Purity?',
         'author': 'ChatGPT & Karan Anand',
-        'article_html_content': """
+        'article_html_content': """# Introduction
 
-        <h2>Introduction</h2>
+Water, the elixir of life. We gulp it down, splash in it, even cook with it, often taking its purity for granted. But lurking beneath its seemingly innocuous surface, a world of complexity unfolds, where unseen contaminants can wreak havoc on our health and well-being. So, what does "pure" water truly mean? And can we ensure we're getting the good stuff?
 
-        <p>Water, the elixir of life. We gulp it down, splash in it, even cook with it, often taking its purity for granted. But lurking beneath its seemingly innocuous surface, a world of complexity unfolds, where unseen contaminants can wreak havoc on our health and well-being. So, what does "pure" water truly mean? And can we ensure we're getting the good stuff?</p>
+# What Exactly Makes Water "Pure"?
 
-        <h2>What Exactly Makes Water "Pure"?</h2>
+Purity is a relative term, especially when it comes to water. In its natural state, water collects minerals, organic matter, and even microorganisms as it flows through rivers, lakes, and underground aquifers. While some of these elements enhance its taste and provide essential nutrients, others can harbor harmful bacteria, heavy metals, or chemical pollutants.
 
-        <p>Purity is a relative term, especially when it comes to water. In its natural state, water collects minerals, organic matter, and even microorganisms as it flows through rivers, lakes, and underground aquifers. While some of these elements enhance its taste and provide essential nutrients, others can harbor harmful bacteria, heavy metals, or chemical pollutants.</p>
+The quest for "pure" water, then, involves removing these unwanted elements to a level deemed safe for human consumption. This process can vary depending on the source and intended use:
 
-        <p>The quest for "pure" water, then, involves removing these unwanted elements to a level deemed safe for human consumption. This process can vary depending on the source and intended use:</p>
-x = mycol.insert_many(mylist)
-        <ul>
-            <li><b>Public water systems</b> treat water using a multi-step approach, typically employing filtration, coagulation, disinfection, and sometimes additional processes like reverse osmosis. This ensures its compliance with national or regional drinking water quality standards.</li>
-            <li><b>Bottled water</b> undergoes filtration and purification processes, which can vary across brands and sources. While often marketed as "pure," it's essential to scrutinize labels and understand the treatment methods used.</li>
-            <li><b>Home filtration systems</b> offer varying levels of purification, from carbon filters targeting chlorine and taste impurities to reverse osmosis systems removing a wider range of contaminants. Choosing the right system depends on your specific water quality concerns and local regulations.</li>
-        </ul>
+- **Public water systems** treat water using a multi-step approach, typically employing filtration, coagulation, disinfection, and sometimes additional processes like reverse osmosis. This ensures its compliance with national or regional drinking water quality standards.
+- **Bottled water** undergoes filtration and purification processes, which can vary across brands and sources. While often marketed as "pure," it's essential to scrutinize labels and understand the treatment methods used.
+- **Home filtration systems** offer varying levels of purification, from carbon filters targeting chlorine and taste impurities to reverse osmosis systems removing a wider range of contaminants. Choosing the right system depends on your specific water quality concerns and local regulations.
+        
+# Beyond Purity: Understanding the Human Factor
 
-        <h2>Beyond Purity: Understanding the Human Factor</h2>
+Even "pure" water isn't a guarantee of absolute safety. Contamination can occur after treatment, during storage, or even within our own homes through pipes and storage containers. It's crucial to practice good hygiene and proper water handling techniques to minimize risks.
 
-        <p>Even "pure" water isn't a guarantee of absolute safety. Contamination can occur after treatment, during storage, or even within our own homes through pipes and storage containers. It's crucial to practice good hygiene and proper water handling techniques to minimize risks.</p>
+## Be a Savvy Water Consumer
 
-        <h3>Be a Savvy Water Consumer</h3>
+- **Be informed:** Familiarize yourself with your local water quality reports and understand the treatment processes used.
+- **Read labels:** Scrutinize bottled water labels to understand the source, purification methods, and mineral content.
+- **Consider home filtration:** If your water quality raises concerns, explore reputable home filtration systems tailored to your needs.
+- **Practice good hygiene:** Wash your hands thoroughly before handling drinking water, and regularly clean containers and storage units.
 
-        <ul>
-            <li><b>Be informed:</b> Familiarize yourself with your local water quality reports and understand the treatment processes used.</li>
-            <li><b>Read labels:</b> Scrutinize bottled water labels to understand the source, purification methods, and mineral content.</li>
-            <li><b>Consider home filtration:</b> If your water quality raises concerns, explore reputable home filtration systems tailored to your needs.</li>
-            <li><b>Practice good hygiene:</b> Wash your hands thoroughly before handling drinking water, and regularly clean containers and storage units.</li>
-        </ul>
-
-        <p>Remember, the water you drink shapes your health. Stay informed, make informed choices, and enjoy the life-giving power of pure water with confidence!</p>
-
+Remember, the water you drink shapes your health. Stay informed, make informed choices, and enjoy the life-giving power of pure water with confidence!
         """,
 
         'table_of_content': [
